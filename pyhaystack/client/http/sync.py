aa08d8532017df1e345df43393b2380cce0034ac
a-mackay <amk@gegroup.com.au>
2020-08-03 14:27:37 +1000

    dbg

--- pyhaystack/client/http/sync.py
+++ pyhaystack/client/http/sync.py
@@ -75,7 +75,7 @@ class SyncHttpClient(HTTPClient):
                         verify=tls_verify,
                         cert=tls_cert,
                     )
-                    print(response)
+                    print(response.text())
                     if (accept_status is None) or (
                         response.status_code not in accept_status
                     ):
